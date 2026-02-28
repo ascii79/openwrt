48c364f9a1c3007c63c25c97dcbd9693c5f614e7
root <root@bpir4.lan>
2026-02-28 17:55:37 +0530

    immich docker changes

--- dockerConfig/immich/startup.sh
+++ dockerConfig/immich/startup.sh
@@ -1,2 +1,3 @@
+curl -fSL -o docker-compose.yml \
+  https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
 docker compose -f docker-compose.yml -f docker-compose.immich.yml -p immich up -d
-
